35199731c89981aaf4d59bfdae9aa21080e39eb8
dacc-code <dickson@semillero.com>
2026-04-09 14:28:26 -0500

    fix: increase minSdk to 24 for camera compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.mangrove_disease_detector"
-        minSdk = 23
+        minSdk = 24
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
